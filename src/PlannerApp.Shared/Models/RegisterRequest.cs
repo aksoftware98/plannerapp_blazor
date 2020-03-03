f33bf3e63352657d225eb8532bc5ef3f30a83d82
aksoftware98 <aksoftware98@hotmail.com>
2020-03-03 22:04:42 +0200

    Implementing Register Components

---
+++ src/PlannerApp.Shared/Models/RegisterRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlannerApp.Shared.Models
+{
+    public class RegisterRequest
+    {
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
---
+++ src/PlannerApp.Shared/Models/UserManagerResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlannerApp.Shared.Models
+{
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public string[] Errors { get; set; }
+        public DateTime? ExpireDate { get; set; }
+    }
+}
